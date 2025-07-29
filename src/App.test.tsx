5135cbce60fcf4d7909fdf5d76f5865e8ba73460
Caleb-Speaker <calebjspeaker@gmail.com>
2025-07-28 17:27:42 -0700

    updated src files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-jest.mock('../firebaseConfig');
+jest.mock('./firebaseConfig');
 
 const queryClient = new QueryClient();
 
