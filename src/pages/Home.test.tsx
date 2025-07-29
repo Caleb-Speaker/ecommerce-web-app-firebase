15cc17e5f5a2302524ceff7ba10cc3c5a646155a
Caleb-Speaker <calebjspeaker@gmail.com>
2025-07-28 17:23:43 -0700

    updated src files

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+jest.mock('../firebaseConfig');
+
 const queryClient = new QueryClient();
 
 describe('Home Page Integration with Redux', () => {
